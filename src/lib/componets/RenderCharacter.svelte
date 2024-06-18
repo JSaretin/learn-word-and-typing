d953d88644caec3062f1cdc342e4f836d18bafde
v0idhack <v0idhack@gmail.com>
2024-06-18 16:31:49 +0100

    update color

--- src/lib/componets/RenderCharacter.svelte
+++ src/lib/componets/RenderCharacter.svelte
@@ -1,16 +1,15 @@
 <script lang="ts">
-
 	export let character: string;
 	export let typedCharacter: string;
 </script>
 
 <span
-			class={' ' +
-				(typedCharacter === undefined
-					? 'text-neutral-300'
-					: typedCharacter === character
-						? 'text-green-300'
-						: character === ' '
-							? 'bg-red-300'
-							: 'text-red-300')}>{character}</span
-		>
+	class={' ' +
+		(typedCharacter === undefined
+			? 'text-neutral-400'
+			: typedCharacter === character
+				? 'text-green-400'
+				: character === ' '
+					? 'bg-red-400'
+					: 'text-red-400')}>{character}</span
+>
